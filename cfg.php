ca62ad8c3f168241db118de03f9eba93a9560738
yocky12k <54408235+yocky12k@users.noreply.github.com>
2019-12-25 22:33:06 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,9 @@
+<?php
+//AMBIL DATA DI BAGIAN REQUEST UNTUK MENDAPATKAN ID DAN API TOKEN
+//id lu
+
+$id = "2498";
+
+//api token lu
+
+$api_token = "keZWjONWNtWtrYBhtVwezxyrVUlD+KDSsAHM89J27/IVhFJL2+4JES9cKh1zGNWOp9how5QbeCVcA6hsNSk1ggFP3xbUhaTxwV6SVgolJOewm6vo4GhdtIVOsVdsrBVZzp5pt9fTZcj7RSJl/m2n+f358I1YB47vCwlWFK3FKhE=";
